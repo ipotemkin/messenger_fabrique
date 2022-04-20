dca6f275b59c08f9b33c0150ac2f38fa677ac889
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-04-21 01:58:51 +0300

    removed python-dotenv, kept django-environ, DEBUG removed to .env

--- messenger/settings.py
+++ messenger/settings.py
@@ -11,34 +11,26 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
-from dotenv import load_dotenv, find_dotenv
-from environ import Env
-
-load_dotenv(find_dotenv())
-
-env = Env()
-env.read_env()
+env = environ.Env(DEBUG=(bool, False))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = env("DEBUG")
 
 PROBE_TOKEN = env("PROBE_TOKEN")
 
-
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
